274f6201f8047d73b4122b16582e1583dabab438
MoNireu <monireu.dev@gmail.com>
2022-01-03 16:41:39 +0900

    Initial Commit

---
+++ Cinnamon/CinnamonApp.swift
@@ -0,0 +1,20 @@
+//
+//  CinnamonApp.swift
+//  Cinnamon
+//
+//  Created by MoNireu on 2022/01/03.
+//
+
+import SwiftUI
+
+@main
+struct CinnamonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
