72d0008aded948d2320019223d82ab426226cccf
MoNireu <monireu.dev@gmail.com>
2022-03-11 11:57:50 +0900

    feat: `ExtractRecipeListView` 추출 방식 Segmented Picker 추가.

--- Cinnamon/Views/ContentViews/ExtractViews/ExtractRecipeListView.swift
+++ Cinnamon/Views/ContentViews/ExtractViews/ExtractRecipeListView.swift
@@ -8,27 +8,41 @@
 import SwiftUI
 
 struct ExtractRecipeListView: View {
+    @State private var extractType: ExtractType = .espresso
     
     var body: some View {
         NavigationView {
-            List {
-                NavigationLink {
-                    EmptyView()
-                } label: {
-                    ExtractRecipeListCell(title: "초간단 브루잉 레시피", description: "1분 30초만에 따라하는 초간단 레시피", time: 90)
+            VStack {
+                Picker("extractType", selection: $extractType) {
+                    Text("에스프레소").tag(ExtractType.espresso)
+                    Text("브루잉").tag(ExtractType.brew)
                 }
-                NavigationLink {
-                    EmptyView()
-                } label: {
-                    ExtractRecipeListCell(title: "초간단 브루잉 레시피", description: "1분 30초만에 따라하는 초간단 레시피", time: 100)
+                .pickerStyle(.segmented)
+                .padding()
+                
+                List {
+                    switch extractType {
+                    case .espresso:
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            ExtractRecipeListCell(title: "초간단 에스프레소 레시피", description: "1분 30초만에 따라하는 초간단 레시피", time: 200)
+                        }
+                    case .brew:
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            ExtractRecipeListCell(title: "초간단 브루잉 레시피", description: "1분 30초만에 따라하는 초간단 레시피", time: 90)
+                        }
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            ExtractRecipeListCell(title: "초간단 브루잉 레시피", description: "1분 30초만에 따라하는 초간단 레시피", time: 100)
+                        }
+                    }
                 }
-                NavigationLink {
-                    EmptyView()
-                } label: {
-                    ExtractRecipeListCell(title: "초간단 브루잉 레시피", description: "1분 30초만에 따라하는 초간단 레시피", time: 200)
-                }
+                .navigationTitle("추출 레시피")
             }
-            .navigationTitle("추출 레시피")
         }
         .listStyle(.plain)
     }
