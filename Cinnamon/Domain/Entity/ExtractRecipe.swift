ce13faae93bf550b270655290ee1e2280fe5d08c
MoNireu <monireu.dev@gmail.com>
2022-03-23 15:55:51 +0900

    refactor: `ExtractRecipeStore`을 `ExtractRecipeRepository`로 변경.

--- Cinnamon/Domain/Entity/ExtractRecipe.swift
+++ Cinnamon/Domain/Entity/ExtractRecipe.swift
@@ -12,67 +12,6 @@ enum ExtractType {
     case brew
 }
 
-class ExtractRecipeStore: ObservableObject {
-    var list: [ExtractRecipe]
-    
-    init() {
-        list = [
-            ExtractRecipe(title: "에스프레소 레시피 1",
-                          description: "1분 에스프레소 레시피",
-                          extractType: .espresso,
-                          totalExtractTime: 60,
-                          beanAmount: 20.0,
-                          steps: [
-                            RecipeStep(title: "뜸 들이기", description: "약 60초 동안 뜸을 들여줍니다.", waterAmount: 40, extractTime: 60),
-                            RecipeStep(title: "1차 푸어링", description: "", waterAmount: 80, extractTime: 60),
-                            RecipeStep(title: "2차 푸어링", description: "", waterAmount: 40, extractTime: 40)
-                        ]),
-            ExtractRecipe(title: "에스프레소 레시피 2",
-                          description: "1분 30초 에스프레소 레시피",
-                          extractType: .espresso,
-                          totalExtractTime: 90,
-                          beanAmount: 20.0,
-                          steps: [
-                            RecipeStep(title: "뜸 들이기", description: "", waterAmount: 40, extractTime: 60),
-                            RecipeStep(title: "1차 푸어링", description: "", waterAmount: 80, extractTime: 60),
-                            RecipeStep(title: "2차 푸어링", description: "", waterAmount: 40, extractTime: 40)
-                        ]),
-            ExtractRecipe(title: "브루잉 레시피 1",
-                          description: "1분 브루잉 레시피",
-                          extractType: .brew,
-                          totalExtractTime: 60,
-                          beanAmount: 20.0,
-                          steps: [
-                            RecipeStep(title: "뜸 들이기", description: "", waterAmount: 40, extractTime: 60),
-                            RecipeStep(title: "1차 푸어링", description: "", waterAmount: 80, extractTime: 60),
-                            RecipeStep(title: "2차 푸어링", description: "", waterAmount: 40, extractTime: 40)
-                        ]),
-            ExtractRecipe(title: "브루잉 레시피 2",
-                          description: "1분 30초 브루잉 레시피",
-                          extractType: .brew,
-                          totalExtractTime: 90,
-                          beanAmount: 20.0,
-                          steps: [
-                            RecipeStep(title: "뜸 들이기", description: "", waterAmount: 40, extractTime: 60),
-                            RecipeStep(title: "1차 푸어링", description: "", waterAmount: 80, extractTime: 60),
-                            RecipeStep(title: "2차 푸어링", description: "", waterAmount: 40, extractTime: 40)
-                        ])
-        ]
-    }
-    
-    
-    func update(_ newRecipe: ExtractRecipe) {
-        if let index = list.firstIndex(where: {$0.id == newRecipe.id}) {
-            print("Log -", #fileID, #function, #line, "Recipe found!")
-            list[index] = newRecipe
-        }
-    }
-    
-    func getRecipeListByExtractType(_ extractType: ExtractType) -> [ExtractRecipe] {
-        return self.list.filter({$0.extractType == extractType})
-    }
-}
-
 class ExtractRecipe: ObservableObject, Identifiable {
     var id: UUID
     var title: String
